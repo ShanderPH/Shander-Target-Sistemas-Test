7e99f0764e67db49f6ea26a7e39a1198738a24c8
ShanderPH <143129391+ShanderPH@users.noreply.github.com>
2024-03-06 03:15:31 -0300

    Update test_4.py

---
+++ test_4.py
@@ -0,0 +1,12 @@
+4)
+
+Primeiro comando: Acender a luz da própria sala
+
+SE a luz acender OU não acender, então:
+    Ir para segunda sala e acender o segundo interruptor.
+    SE a luz acender na segunda sala AND tiver acendido na primeira sala, então:
+        Identificado 3 lâmpadas, estão cada um em sua respectiva sala.
+    SE NÃO , SE a luz na segunda sala não acender AND tiver acendido na primeira sala, então:
+        Identificado 3 lâmpadas, a primeira na sala 1, a segunda na sala 3 e a terceira na sala 2.
+    SE NÃO
+        Identificado 3 lâmpadas, a primeira na sala 3, a segunda na sala 1 e a terceira na sala 2.
